a8bf26780fa33237e272a64e507b73bea985c50f
momo-abcd <yym4481@naver.com>
2021-06-11 12:56:20 +0900

    6_11

--- app/src/main/java/com/hfad/spotify/MainActivity.java
+++ app/src/main/java/com/hfad/spotify/MainActivity.java
@@ -26,8 +26,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<MusicList> list;
-    private TextView asdf;
-    private ImageView elbum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,28 +33,12 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
 
-        asdf = (TextView) findViewById(R.id.http);
-        elbum = (ImageView) findViewById(R.id.elbumImg);
-//        list = new ArrayList<>();
-//        list.add(new MusicList("asd","1","Butter","방탄소년단"));
-//        list.add(new MusicList("asd","2","헤픈 우연","헤이즈 (Heize)"));
-//        list.add(new MusicList("asd","3","Dun Dun Dance","오마이걸 (OH MY GIRL)"));
-//        list.add(new MusicList("asd","4","Next Level","aespa"));
-//        list.add(new MusicList("asd","5","Peaches (Feat.Daniel Caesar & Giveon)","Justin Bieber"));
 
-//        SimpleTextAdapter adapter = new SimpleTextAdapter(list);
+
         RecyclerView recyclerView = findViewById(R.id.recycler1);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView.setAdapter(adapter);
-
-//        asdf.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                new HttpUtil(recyclerView).execute();
-//            }
-//        });
-            new HttpUtil(recyclerView).execute();
+        new HttpUtil(recyclerView).execute();
 
 
         }
